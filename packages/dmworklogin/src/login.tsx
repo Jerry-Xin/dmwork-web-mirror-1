4b9942aae810a8faa5cc4e6baa8f2f2009cfffcb
lizhiwei <will.li@miaozhen.com>
2026-03-26 10:46:23 +0800

    fix: restore Spin import, used by QR code loading

--- packages/dmworklogin/src/login.tsx
+++ packages/dmworklogin/src/login.tsx
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import { Button, Toast } from '@douyinfe/semi-ui';
+import { Button, Spin, Toast } from '@douyinfe/semi-ui';
 import './login.css'
 import QRCode from 'qrcode.react';
 import { WKApp, Provider } from "@dmwork/base"
